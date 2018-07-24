bc8e0444b065a24570efed0f7f792d0b5d1989df
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2018-07-24 16:52:34 -0400

    First component working

--- src/ViveTrack/Start.cs
+++ src/ViveTrack/Start.cs
@@ -3,30 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Autodesk.DesignScript.Runtime;
 using Autodesk.DesignScript.Interfaces;
 using Autodesk.DesignScript.Geometry;
 
+using Valve.VR;
 
-    public class Start
+using ViveTrack;
+
+public class Start
+{
+    internal Start() { }
+
+    public static OpenvrWrapper Vive = new OpenvrWrapper();
+    public static CoordinateSystem CalibrationTransform = CoordinateSystem.Identity();
+    public static Plane CalibrationPlane;
+    public static string OutMsg;
+
+
+    /// <summary>
+    /// Start HTC Vive. Make sure SteamVR is running, and Dynamo is set to Periodic update.
+    /// </summary>
+    /// <param name="Connect">Connect to SteamVR?</param>
+    /// <returns></returns>
+    [CanUpdatePeriodically(true)]
+    [MultiReturn(new[] { "Msg", "Vive" })]
+    public static object ConnectToVive(bool Connect)
     {
-        internal Start() { }
+        if (!Connect) return null;
 
-
-        /// <summary>
-        /// Starts Vive tracking...
-        /// </summary>
-        /// <returns></returns>
-        [CanUpdatePeriodically(true)]
-        public static object StartVive()
+        if (!DetectSteamVR())
         {
+            DynamoServices.LogWarningMessageEvents.OnLogWarningMessage("SteamVR not running.Please run SteamVR first.");
             return null;
         }
 
-        public static object CalibrateOrigin()
-        {
-            return null;
-        }
+        OutMsg = "Connected!";
+
+
+        return new Dictionary<string, object> {
+            { "Msg", OutMsg },
+            { "Vive", Vive }
+        };
     }
+
+
+    internal static bool DetectSteamVR()
+    {
+        Process[] vrServer = Process.GetProcessesByName("vrserver");
+        Process[] vrMonitor = Process.GetProcessesByName("vrmonitor");
+        if ((vrServer.Length == 0) || (vrMonitor.Length == 0)) return false;
+        if (!OpenVR.IsRuntimeInstalled()) return false;
+
+        return true;
+    }
+
+
+
+
+    public static object CalibrateOrigin()
+    {
+        return null;
+    }
+}
 
